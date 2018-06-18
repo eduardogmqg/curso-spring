c6a13966eb4366b02b4a921fd461467303d9309a
eduardogm81qg <eduardogmqg@gmail.com>
2018-06-18 12:18:02 +0200

    Spring IoC
    Primer commit sobre IoC. Primera parte sin usar Spring

---
+++ spring-ioc/src/main/java/space/pentaquark/springdemo/BaseballCoach.java
@@ -0,0 +1,9 @@
+package space.pentaquark.springdemo;
+
+public class BaseballCoach {
+
+    public String getDailyWorkout() {
+        return "Spend 30 minutes on batting practice";
+    }
+
+}
---
+++ spring-ioc/src/main/java/space/pentaquark/springdemo/MyApp.java
@@ -0,0 +1,12 @@
+package space.pentaquark.springdemo;
+
+public class MyApp {
+
+    public static void main(String[] args) {
+        // Create the Object
+        BaseballCoach theCoach = new BaseballCoach();
+        // Use the object
+        System.out.println(theCoach.getDailyWorkout());
+    }
+
+}
