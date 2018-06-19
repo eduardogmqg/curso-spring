06905f83d39c6aa3f7313c060b5e37e9c6aa7885
eduardogm81qg <eduardogmqg@gmail.com>
2018-06-19 09:48:40 +0200

    Spring DI XML
    Ejemplo de inyección de valores literales.

--- spring-ioc/src/main/java/space/pentaquark/springdemo/CricketCoach.java
+++ spring-ioc/src/main/java/space/pentaquark/springdemo/CricketCoach.java
@@ -5,6 +5,10 @@ public class CricketCoach implements Coach{
 
     private FortuneService fortuneService;
 
+    private String emailAddress;
+    private String team;
+
+
     // Create a no-args constructor
     public CricketCoach() {
         System.out.println("CricketCoach: Inside no-args constructor");
@@ -20,7 +24,25 @@ public class CricketCoach implements Coach{
 
     // our setter method
     public void setFortuneService(FortuneService fortuneService) {
-        System.out.println("CricketCoach: Inside setter method");
+        System.out.println("CricketCoach: Inside setter method - setFortuneService");
         this.fortuneService = fortuneService;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        System.out.println("CricketCoach: Inside setter method - setEmailAddress");
+        this.emailAddress = emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        System.out.println("CricketCoach: Inside setter method - setTeam");
+        this.team = team;
     }
 }
--- spring-ioc/src/main/java/space/pentaquark/springdemo/SetterDemoApp.java
+++ spring-ioc/src/main/java/space/pentaquark/springdemo/SetterDemoApp.java
@@ -11,11 +11,19 @@ public class SetterDemoApp {
                 new ClassPathXmlApplicationContext("applicationContext.xml");
 
         // retrieve bean for spring container
-        Coach theCoach = context.getBean("myCricketCoach", Coach.class);
+        // Coach theCoach = context.getBean("myCricketCoach", Coach.class);
+
+        // Nota: Para usar las propiedades emailAdress y team, el objeto tiene que ser
+        // de tipo CricketCoack ya que son propiedades específicas de esa clase
+        CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
 
         // call method on the bean
         System.out.println(theCoach.getDailyWorkout());
         System.out.println(theCoach.getDailyFortune());
+
+        // call our new method to get the literal values
+        System.out.println(theCoach.getEmailAddress());
+        System.out.println(theCoach.getTeam());
 
         // close the context
         context.close();
